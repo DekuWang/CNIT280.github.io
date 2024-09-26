0cd55d8f5b69c31711b85ef6fef2b6b396a4bf2f
DekuWang <2873450572@qq.com>
2024-09-26 10:30:07 -0400

    update the gitignore

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-var userDB = {
-    "admin": {
-        "password": "admin",
-        "Username": "Admin"},
-    "user1": {
-        "password": "user1",
-        "Username": "User1"},
-};
-
 function validateLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    // Simple validation logic
-    if (userDB[username] && userDB[username]["password"] == password) {
-        alert("login_success");
-        redirection(userDB[username]["Username"]);
-    } else {
-        alert("login_fail");
+
+    fetch("./users.json")
+    .then(response => response.json())
+    .then(data => {
+        const userDB = data;
+
+        // Simple validation logic
+        if (userDB[username] && userDB[username]["password"] == password) {
+            alert("login_success");
+            redirection(userDB[username]["shown_name"]);
+        } else {
+            alert("login_fail");
+        }
     }
-}
+)}
 function redirection(username = NaN){
     window.location.href = `after_login.html?username=${username}`;
+}
+
+function test(){
+    fetch("./users.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data["admin"]["password"]);
+    })
 }
